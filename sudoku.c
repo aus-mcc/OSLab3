3c17158ac1010d41980714c7554ed8f1824d8e95
Austin McCulloch <austinjames.mcculloch@uoit.net>
2018-03-09 23:22:33 -0500

    commented commit

--- sudoku.c
+++ sudoku.c
@@ -6,14 +6,17 @@
 #define COL_THREADS 1
 #define SUB_THREADS 9
 
-int board[9][9];
-int solution[11];
+int board[9][9]; // sudoku board
+int solution[11]; // array to hold returns from funcs
 
+// struct to hold the start coordinated for the sub grid check
 typedef struct{
     int row;
     int column;
 } box;
 
+
+// function to read the file
 void readFile(FILE *fp){
     for(int i=0;i<9;i++){
         for(int j=0;j<9;j++){
@@ -21,15 +24,17 @@ void readFile(FILE *fp){
         }
     }
 }
+
+// Check Row
 void *rowCheck(){
-    int *isValid = (int*)1;
-    for(int r=0;r<9;r++){
-        int used[9]={0};
-        for(int v=0;v<9;v++){
-            if(used[board[r][v]-1]==0){
+    int *isValid = (int*)1; // if returns 1 the rows are valid
+    for(int r=0;r<9;r++){ // for each row
+        int used[9]={0}; // array to see which numbers are used
+        for(int v=0;v<9;v++){ // check each element
+            if(used[board[r][v]-1]==0){ // set element as used
                 used[board[r][v]-1] =1;
             }
-            else{
+            else{ // if element has been used before
                 isValid = 0;
                 break;
             }
@@ -37,16 +42,18 @@ void *rowCheck(){
     }
     return isValid;
 }
+
+// COlumn Check
 void *colCheck(){
-   int *colValid = (int*)1;
-   for(int c=0;c<9;c++){
-       int colUsed[9] = {0};
-       for(int b=0;b<9;b++){
-           int cur = board[c][b];
-           if(colUsed[cur-1]==0){
-               colUsed[cur-1] = 1;
+   int *colValid = (int*)1; // Return value for the function
+   for(int c=0;c<9;c++){ // for each column
+       int colUsed[9] = {0}; // array to see which numbers are used
+       for(int b=0;b<9;b++){ // for each element in the column
+           int cur = board[b][c];
+           if(colUsed[cur-1]==0){ //if value has not been used
+               colUsed[cur-1] = 1; // set it ot used
            }
-           else{
+           else{ // else value has been used
                colValid = 0;
                break;
            }
@@ -54,22 +61,24 @@ void *colCheck(){
     }
     return colValid;
 }
+
+// Check Subgrid
 void *subCheck(void * grid){
-    int *boxValid = (int*)1;
+    int *boxValid = (int*)1; // Return value for function
     int pos;
-    int boxVal[9] = {0};
-    box *sub = grid; 
-    int gcol = sub->column;
+    int boxVal[9] = {0}; // array to see which numbers are used
+    box *sub = grid; // make box struct
+    int gcol = sub->column; // pass the values into struct
     int grow = sub->row;
 
 
-    for(int col=0;col<3;col++){
-        for(int row=0;row,3;row++){
-            pos = board[row+grow][col+gcol];
-            if(boxVal[pos -1]==0){
-                boxVal[pos -1] =1;
+    for(int col=0;col<3;col++){ // for each column in box
+        for(int row=0;row,3;row++){ // for each row in box
+            pos = board[row+grow][col+gcol]; //start index
+            if(boxVal[pos -1]==0){ // if value has not been used
+                boxVal[pos -1] =1; // set  it as used
             }
-            else{
+            else{ // else value has been used
                 boxValid = 0;
                 break;
             }
@@ -78,8 +87,10 @@ void *subCheck(void * grid){
     return boxValid;
 }
 
-int check(int a[]){
+int check(int a[]){ //function to check solution array
     int fullValid = 1;
+
+    // Loop that checks to see if all elements in the array are the same
     for(int i=0;i<8;i++){
         if(a[i] != a[i+1]){
             fullValid = 0;
@@ -92,15 +103,18 @@ int check(int a[]){
         
 }
 int main(){
-    FILE *fp = fopen("puzzle.txt", "r");
-    readFile(fp);
+    FILE *fp = fopen("puzzle.txt", "r"); // Open file
+    readFile(fp); // call readFile function
 
+
+    // Define the threads
     pthread_t r_thd;
     pthread_t c_thd;
     pthread_t b_thd[9];
 
-    box box[9];
+    box box[9]; // create box struct
 
+    // Create all threads
     pthread_create(&r_thd, 0, rowCheck, (void*) "Check Rows");
     pthread_create(&c_thd, 0, colCheck, (void*) "Column Check");
     int i=0;
@@ -111,6 +125,8 @@ int main(){
             pthread_create(&b_thd[i], 0, subCheck, &box[i]);
         }
     }
+
+    // Join all threads and have the return values entered into the solutions array
     for(int j=0;j<3;j++){
         void *solution[j];
         if(j==0){
@@ -123,13 +139,13 @@ int main(){
             pthread_join(b_thd[i],&solution[j]);
         }
     }
-
+    // Check if all elements in solution array are the same
     int isCorrect = check(solution);
     if(isCorrect){
-        printf("Solution is valid");
+        printf("Solution is valid\n");
     }
     else{
-        printf("Solution is not valid");
+        printf("Solution is not valid\n");
     }
 
 
